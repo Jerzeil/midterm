bc0fa54afd5193ffc6742f878664d949ef265b5e
Jerzeil <jerzylira@gmail.com>
2020-11-04 09:56:09 +0800

    repo start

---
+++ miniexpress.js
@@ -0,0 +1,10 @@
+const http = require(`http`)
+
+const server = http.createServer((request,response) => {
+
+});
+/** 
+server.listen(5000,() => {
+    console.log(`Server is starting at: http://localhost:5000`);
+});
+*/
